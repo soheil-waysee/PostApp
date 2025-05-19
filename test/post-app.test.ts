e35578b58dd0003c50249dfab9adc98e9309cc11
SoheilWaysee <soheilwaysee@gmail.com>
2025-05-19 21:29:51 +0200

    Initial commit

---
+++ test/post-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PostApp from '../lib/post-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/post-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PostApp.PostAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
